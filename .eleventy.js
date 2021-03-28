4ac311d1e5200a2b941210f030758e1de4e312f7
David B. Waters <mrdavidbwaters@gmail.com>
2021-03-28 17:32:20 -0400

    remove 11ty asset copying since snowpack mounts these

--- .eleventy.js
+++ .eleventy.js
@@ -35,7 +35,7 @@ module.exports = function (eleventyConfig) {
     ...formats.assets
   ])
 
-  eleventyConfig.addPassthroughCopy('assets')
+  // eleventyConfig.addPassthroughCopy('assets')
 
   return config
 }
